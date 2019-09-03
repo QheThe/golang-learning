af24eb12b5d8b1792c0d64e2dc0ecba966b0295d
quartz <qhethe525@gmail.com>
2019-09-03 15:02:33 +0800

    feat: 多层循环break

--- loop/for_loop.go
+++ loop/for_loop.go
@@ -18,8 +18,23 @@ func while_loop() {
 	}
 }
 
+// break_inner - break 语句只会跳出单层循环
+func break_inner() {
+	// 循环 3次 输出一次
+	for i :=0; i<3; i++ {
+		// 阻塞 j < 10 输出10次
+		for j :=0; j<10; j++ {
+			fmt.Print(j)
+		}
+		// 输出空格 分行
+		fmt.Print("  ")
+	}
+	fmt.Println("")
+}
+
 // For_loop - for 循环
 func For_loop() {
 	fmt.Println("++++++ for 循环 +++++++")
 	while_loop()
+	break_inner()
 }
