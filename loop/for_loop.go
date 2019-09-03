2e9e5c696f8367567a2b34098c347441984f756d
quartz <qhethe525@gmail.com>
2019-09-03 15:11:02 +0800

    feat: continue 语句

--- loop/for_loop.go
+++ loop/for_loop.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func continue_section() {
+	for i:=0; i<10; i++ {
+		if i == 5 {
+			// 跳过当前循环
+			fmt.Println("skip 5")
+			continue
+		}
+		fmt.Print(i)
+		fmt.Print("  ")
+	}
+}
+
 func while_loop() {
 	var i int
 	// 无条件 for 等价于 while true
@@ -16,6 +28,7 @@ func while_loop() {
 			break
 		}
 	}
+	fmt.Println("")
 }
 
 // break_inner - break 语句只会跳出单层循环
@@ -37,4 +50,5 @@ func For_loop() {
 	fmt.Println("++++++ for 循环 +++++++")
 	while_loop()
 	break_inner()
+	continue_section()
 }
